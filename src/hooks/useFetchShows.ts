aeca1149b93673702cd7e47e30e4574efdba0e34
Mayeul-T <mayeul.touchard@polyconseil.fr>
2020-06-05 12:22:16 +0200

    Update hook without import graphql

--- src/hooks/useFetchShows.ts
+++ src/hooks/useFetchShows.ts
@@ -1,7 +1,29 @@
 import { useState, useEffect, SetStateAction, Dispatch } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import { ShowDto } from '../types';
-import query from '../utils';
+
+const query = graphql`
+  {
+    allInternalShows(filter: { id: { ne: "dummy" } }) {
+      edges {
+        node {
+          alternative_id
+          id
+          account_id
+          title
+          slug
+          featuredCover {
+            childImageSharp {
+              fluid(maxWidth: 1500) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 const useFetchShows = (): [ShowDto[], Dispatch<SetStateAction<ShowDto[]>>] => {
   const data = useStaticQuery(query);
