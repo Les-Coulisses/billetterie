c38a5d240e863fe95a2ba8e2e0e4a91debd52d88
Mayeul-T <mayeul.touchard@polyconseil.fr>
2020-06-16 01:05:45 +0200

    MAJ dialog avec rounded + ajout boutton

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Dialog as MuiDialog } from '@material-ui/core';
 
 import { DialogProps } from '../types';
+import '../scss/dialog.scss';
 
 export default function Dialog(dialogProps: DialogProps) {
-  const classes =
+  let classes =
     dialogProps.className === undefined ? '' : dialogProps.className;
+  classes += dialogProps.rounded === true ? ' CoulissesRoundedDialog ' : '';
   return (
     <MuiDialog
       {...{
--- src/components/types.ts
+++ src/components/types.ts
@@ -1,16 +1,19 @@
 import { StepperProps as MuiStepperProps } from '@material-ui/core/Stepper';
 import { ButtonProps as MuiButtonProps } from '@material-ui/core/Button';
 import { DialogProps as MuiDialogProps } from '@material-ui/core/Dialog';
+import { ModalProps as MuiModalProps } from '@material-ui/core/Modal';
 import { ToolbarProps as MuiToolbarProps } from '@material-ui/core/Toolbar';
 
 export interface ButtonProps extends MuiButtonProps {}
-export interface DialogProps extends MuiDialogProps {}
+export interface DialogProps extends MuiDialogProps {
+  rounded?: boolean;
+}
 export interface ToolbarProps extends MuiToolbarProps {}
 
 export type Step = {
-  label: string | undefined;
+  label?: string;
   component: React.ReactElement;
-  disabled: boolean | undefined;
+  disabled?: boolean;
 };
 
 export interface StepperProps extends MuiStepperProps {
