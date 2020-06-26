b9585efca12327e2b13d11c2fda1ff9b5fdf7198
Mayeul-T <mayeul.touchard@polyconseil.fr>
2020-06-27 01:10:22 +0200

    Affichage des affiches des spectacles

---
+++ src/components/ModalOrder/Steps/ShowsStep/ShowItem.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Img from 'gatsby-image';
+import { ShowDto } from '../../../../types';
+import { useOrderContext } from '../../../../hooks/OrderContext';
+import { getPerformances, getCategories } from '../../../../utils';
+
+interface ShowsItemProps {
+  show: ShowDto;
+}
+
+export default function ShowsItem({ show }: ShowsItemProps) {
+  const [order, setOrder, activeStep, setActiveStep] = useOrderContext();
+
+  const handleOnClick = () => {
+    setOrder({ ...order, show: show });
+    const performances = getPerformances(show);
+    if (performances.length === 1) {
+      const performance = performances[0];
+      const categories = getCategories(performance);
+      if (categories.length === 1) {
+        setOrder({
+          ...order,
+          show: show,
+          performance: performance,
+          category: categories[0]
+        });
+      } else {
+        setOrder({
+          ...order,
+          show: show,
+          performance: performance,
+          category: undefined
+        });
+      }
+    }
+    setActiveStep(activeStep + 1);
+  };
+  return (
+    <div onClick={handleOnClick}>
+      <Img fluid={show.featuredCover?.childImageSharp.fluid} />
+    </div>
+  );
+}
